0666e2f6d74afb0ffd5230c4d6260fb2eef0730f
Timm McLaughlin <timmothymclaughlin@gmail.com>
2021-03-26 20:37:01 -0500

    Adding Home Fragment details

--- app/src/main/java/com/nedeveloper/billman/ui/home/HomeViewModel.java
+++ app/src/main/java/com/nedeveloper/billman/ui/home/HomeViewModel.java
@@ -1,10 +1,21 @@
 package com.nedeveloper.billman.ui.home;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LifecycleOwner;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 
+import java.util.List;
+
 public class HomeViewModel extends ViewModel {
+
+
+    private LiveData<List<Object>> budgetItem;
+
+
+    /*
 
     private MutableLiveData<String> mText;
 
@@ -15,5 +26,5 @@ public class HomeViewModel extends ViewModel {
 
     public LiveData<String> getText() {
         return mText;
-    }
+    }*/
 }
